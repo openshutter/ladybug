6648f2f48e5056b66b53b1ff5dfb0807edaecbc8
Edwin Pers <pers.edwin@gmail.com>
2023-01-22 13:31:11 -0500

    some wip

--- firmware/ladybug/include/led.h
+++ firmware/ladybug/include/led.h
@@ -1,28 +1,83 @@
 #ifndef LED_H_
 #define LED_H_
 
-/*
+/**
  * Blinks the given led
- * Blinks led on for "time"
- * Then blocks for "off"
- * Repeating "repeat" times
+ *
+ * @param time        on time per repeat
+ * @param repeat      number of times to repeat
+ * @param off         off time between repeat
+ * @param led         which led pin to use
+ * @param activeLow   led pin active low instead of high (common anode)
  */
-void blinkLed(int time = 50, int repeat = 1, int off = 0, int led = LED_BUILTIN) {
-  bool activeLow = true;
-  if (led != LED_BUILTIN) {
-    activeLow = false;
+void blinkLed (
+    int  time      = 100,
+    int  repeat    = 1,
+    int  off       = 0,
+    int  led       = LED_BUILTIN,
+    bool activeLow = true ) {
+  pinMode ( led, OUTPUT );
+  // Set a default off time of 50 if they specified repeat but not dwell
+  if ( repeat > 1 && off == 0 ) {
+    off = 50;
   }
-  while(repeat > 0) {
+
+  while ( repeat > 0 ) {
     // led off
-    activeLow ? digitalWrite(led, HIGH) : digitalWrite(led, LOW);
+    activeLow ? digitalWrite ( led, HIGH ) : digitalWrite ( led, LOW );
     // led on
-    activeLow ? digitalWrite(led, LOW) : digitalWrite(led, HIGH);
-    // wait, led on
-    delay(time);
+    activeLow ? digitalWrite ( led, LOW ) : digitalWrite ( led, HIGH );
+    // wait with led on
+    delay ( time );
     // led off
-    activeLow ? digitalWrite(led, HIGH) : digitalWrite(led, LOW);
-    // wait, led off
-    delay(off);
+    activeLow ? digitalWrite ( led, HIGH ) : digitalWrite ( led, LOW );
+    // wait with led off
+    delay ( off );
+    repeat--;
+  }
+}
+
+/**
+ * Breathes the given led
+ *
+ * @param time        on time per repeat
+ * @param repeat      number of times to repeat
+ * @param off         off time between repeat
+ * @param led         which led pin to use
+ * @param activeLow   led pin active low instead of high (common anode)
+ */
+void breatheLed (
+    int  time      = 1000,
+    int  repeat    = 1,
+    int  off       = 0,
+    int  led       = LED_BUILTIN,
+    bool activeLow = true ) {
+  pinMode ( led, OUTPUT );
+  float stepDuration = ( ( time / 2 ) / 255 );
+  while ( repeat > 0 ) {
+    if ( activeLow ) {
+      for ( int i = 255; i > 0; i-- ) {
+        analogWrite ( led, i );
+        delay ( stepDuration );
+      }
+      for ( int i = 0; i < 255; i++ ) {
+        analogWrite ( led, i );
+        delay ( stepDuration );
+      }
+      activeLow ? digitalWrite ( led, HIGH ) : digitalWrite ( led, LOW );
+    } else {
+      for ( int i = 0; i < 255; i++ ) {
+        analogWrite ( led, i );
+        delay ( stepDuration );
+      }
+      for ( int i = 255; i > 0; i-- ) {
+        analogWrite ( led, i );
+        delay ( stepDuration );
+      }
+      activeLow ? digitalWrite ( led, HIGH ) : digitalWrite ( led, LOW );
+    }
+    // dwell
+    delay ( off );
     repeat--;
   }
 }
--- firmware/ladybug/ladybug.ino
+++ firmware/ladybug/ladybug.ino
@@ -1,31 +1,29 @@
 // Includes
 #include "include/led.h"
-#include "include/setup.h"
 #include "include/output.h"
+#include "include/setup.h"
 
 // the setup function runs once when you press reset or power the board
-void setup() {
+void setup () {
   // Setup outputs
-  initSetupOutputs();
+  initSetupOutputs ();
   // Setup BLE
-  initSetupBle();
+  initSetupBle ();
 
   // Wait for serial port to show up before continuing
-  Serial.begin(115200);
+  Serial.begin ( 115200 );
   while ( !Serial ) {
-    blinkLed(200, 2);
-    delay(400);
+    blinkLed ();
+    delay ( 100 );
   }
-  Serial.println("===============");
-  Serial.println("= Openshutter =");
-  Serial.println("= Ladybug     =");
-  Serial.println("= v0.0.1      =");
-  Serial.println("===============");
+  Serial.println ( "===============" );
+  Serial.println ( "= Openshutter =" );
+  Serial.println ( "= Ladybug     =" );
+  Serial.println ( "= v0.0.1      =" );
+  Serial.println ( "===============" );
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  // do a normal trigger
-  triggerOutput();
-  delay(5000);
+void loop () {
+  breatheLed ( 2000, 1, 2000 );
 }
